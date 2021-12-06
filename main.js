a37ca2634657a6390821e753697c0382c58b9fe3
Sayandip Halder <sayandiph4@gmail.com>
2021-12-06 20:33:01 +0530

    setup firebase and media sources

--- main.js
+++ main.js
@@ -1,6 +1,66 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyAPh9Lu6f8x12I5fM01L6Z9NKJfKJiPGv4",
+  authDomain: "supervid-ba365.firebaseapp.com",
+  projectId: "supervid-ba365",
+  storageBucket: "supervid-ba365.appspot.com",
+  messagingSenderId: "557416888395",
+  appId: "1:557416888395:web:cfd3c46188a62ab279b5ce",
+  measurementId: "G-E3HZYDL4GK"
+});
+
+const firestore = firebase.firestore();
+
+const servers = {
+  iceServers: [
+    {
+      urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
+    },
+  ],
+  iceCandidatePoolSize: 10,
+};
+
+
+// Global State
+const pc = new RTCPeerConnection(servers);
+let localStream = null;
+let remoteStream = null;
+
+// HTML elements
+const webcamButton = document.getElementById('webcamButton');
+const webcamVideo = document.getElementById('webcamVideo');
+const callButton = document.getElementById('callButton');
+const callInput = document.getElementById('callInput');
+const answerButton = document.getElementById('answerButton');
+const remoteVideo = document.getElementById('remoteVideo');
+const hangupButton = document.getElementById('hangupButton');
+
+// 1. Setup media sources
+
+webcamButton.onclick = async () => {
+  localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+  remoteStream = new MediaStream();
+
+  // Push tracks from local stream to peer connection
+  localStream.getTracks().forEach((track) => {
+    pc.addTrack(track, localStream);
+  });
+
+  // Pull tracks from remote stream, add to video stream
+  pc.ontrack = (event) => {
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+  };
+
+  webcamVideo.srcObject = localStream;
+  remoteVideo.srcObject = remoteStream;
+
+  callButton.disabled = false;
+  answerButton.disabled = false;
+  webcamButton.disabled = true;
+};
